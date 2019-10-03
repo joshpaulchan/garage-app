5a96e3d9630e6e3f13a929aa72cd6316323fba6d
Joshua Paul Chan <joshpaulchan@gmail.com>
2019-10-03 17:04:58 -0400

    Centralize get_cluster

--- applications/services.py
+++ applications/services.py
@@ -13,12 +13,7 @@ class DevstackApplicationService(ApplicationService):
         self.devstack_cache = {}
 
     def get_applications_for_cluster(self, cluster_id):
-        if cluster_id not in self.devstack_cache:
-            devstack = Deployment(cluster_id)  # cluster_id = devstack_name
-            devstack.collect_apps()
-            self.devstack_cache[cluster_id] = devstack
-
-        devstack = self.devstack_cache[cluster_id]
+        devstack = self._get_cluster(cluster_id)
         services_and_versions = devstack.get_versions(current_only=True)
 
         return list(
@@ -32,3 +27,10 @@ class DevstackApplicationService(ApplicationService):
         print(f"deploying targets: {deploy_targets}")
 
         return deploy_targets
+
+    def _get_cluster(self, cluster_id):
+        if cluster_id not in self.devstack_cache:
+            devstack = Deployment(cluster_id)  # cluster_id = devstack_name
+            devstack.collect_apps()
+            self.devstack_cache[cluster_id] = devstack
+        return self.devstack_cache[cluster_id]
